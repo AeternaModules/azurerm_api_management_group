de70e7de7203e7a830ed516fbfcca608096e874e
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:15:27 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,33 +1,29 @@
-output "api_management_groups" {
-  description = "All api_management_group resources"
-  value       = azurerm_api_management_group.api_management_groups
-}
 output "api_management_groups_api_management_name" {
-  description = "List of api_management_name values across all api_management_groups"
-  value       = [for k, v in azurerm_api_management_group.api_management_groups : v.api_management_name]
+  description = "Map of api_management_name values across all api_management_groups, keyed the same as var.api_management_groups"
+  value       = { for k, v in azurerm_api_management_group.api_management_groups : k => v.api_management_name }
 }
 output "api_management_groups_description" {
-  description = "List of description values across all api_management_groups"
-  value       = [for k, v in azurerm_api_management_group.api_management_groups : v.description]
+  description = "Map of description values across all api_management_groups, keyed the same as var.api_management_groups"
+  value       = { for k, v in azurerm_api_management_group.api_management_groups : k => v.description }
 }
 output "api_management_groups_display_name" {
-  description = "List of display_name values across all api_management_groups"
-  value       = [for k, v in azurerm_api_management_group.api_management_groups : v.display_name]
+  description = "Map of display_name values across all api_management_groups, keyed the same as var.api_management_groups"
+  value       = { for k, v in azurerm_api_management_group.api_management_groups : k => v.display_name }
 }
 output "api_management_groups_external_id" {
-  description = "List of external_id values across all api_management_groups"
-  value       = [for k, v in azurerm_api_management_group.api_management_groups : v.external_id]
+  description = "Map of external_id values across all api_management_groups, keyed the same as var.api_management_groups"
+  value       = { for k, v in azurerm_api_management_group.api_management_groups : k => v.external_id }
 }
 output "api_management_groups_name" {
-  description = "List of name values across all api_management_groups"
-  value       = [for k, v in azurerm_api_management_group.api_management_groups : v.name]
+  description = "Map of name values across all api_management_groups, keyed the same as var.api_management_groups"
+  value       = { for k, v in azurerm_api_management_group.api_management_groups : k => v.name }
 }
 output "api_management_groups_resource_group_name" {
-  description = "List of resource_group_name values across all api_management_groups"
-  value       = [for k, v in azurerm_api_management_group.api_management_groups : v.resource_group_name]
+  description = "Map of resource_group_name values across all api_management_groups, keyed the same as var.api_management_groups"
+  value       = { for k, v in azurerm_api_management_group.api_management_groups : k => v.resource_group_name }
 }
 output "api_management_groups_type" {
-  description = "List of type values across all api_management_groups"
-  value       = [for k, v in azurerm_api_management_group.api_management_groups : v.type]
+  description = "Map of type values across all api_management_groups, keyed the same as var.api_management_groups"
+  value       = { for k, v in azurerm_api_management_group.api_management_groups : k => v.type }
 }
 
